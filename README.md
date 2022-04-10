5df1c8dd87884c2d1c19ce4f9e9490aab96d765d
Alina Rallo <alina.korzik95@mail.ru>
2022-04-10 19:02:35 +0300

    download_music_success

---
+++ README.md
@@ -0,0 +1,5 @@
+# Music downloader from Youtube
+
+## How to use?
+
+Use `node index.js url` to run the programm. You can download several audiofiles in one time `node index.js url[1] url[2]`
--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const process = require('process')
+const { argv } = require('process');
+const process = require('process');
+const ytdl = require('ytdl-core');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+async function downloadVideo (arr) {
+
+    for await(const video of arr) {
+
+        console.log(video)
+
+        let info = await ytdl.getInfo(video);
+        let downloadedInfo = await ytdl.downloadFromInfo(info);
+        let stream = fs.createWriteStream('/home/alina/node/video/audio'+ Math.random() + '.mp3');
+            
+        await ffmpeg(downloadedInfo)
+            .audioBitrate(info.formats[0].audioBitrate)
+            .withAudioCodec('libmp3lame')
+            .toFormat('mp3')
+            .saveToFile(stream)
+    }
+}
+
+(function createVideoArr (arr) {
+
+    let videoArr = []
+    for(let i=2; i<arr.length; i++) {
+
+        videoArr.push(arr[i])
+    }
+    console.log(videoArr)
+    downloadVideo(videoArr)
+})(argv)
